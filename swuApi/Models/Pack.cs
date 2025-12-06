09e7f4374e1bc85a066b7f8577ecaa070b174ff9
sergioLahuerta <64411350+sergioLahuerta@users.noreply.github.com>
2025-12-07 00:48:42 +0100

    Pack Model added

--- swuApi/Models/Pack.cs
+++ swuApi/Models/Pack.cs
@@ -9,7 +9,7 @@ namespace swuApi.Models
         public int Id { get; set; } 
         public string PackName { get; set; } = string.Empty;
         public int NumberOfCards { get; set; } = 16;
-        public int ShowcaseRarityOdds { get; set; } 
+        public int ShowcaseRarityOdds { get; set; }
         public bool GuaranteesRare { get; set; } = true;
         public decimal Price { get; set; } = 4.99M;
         public DateTime ReleaseDate { get; set; }
