f580bb0c4e7a7ff16743570c3c0f432b2f56c4be
sergioLahuerta <64411350+sergioLahuerta@users.noreply.github.com>
2025-12-06 13:04:41 +0100

    Add IService interface for generic service operations

---
+++ app/Services/IService.cs
@@ -0,0 +1,13 @@
+namespace swuApi.Services
+{
+    public interface IService<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task InicializarDatosAsync();
+
+    }
+}
