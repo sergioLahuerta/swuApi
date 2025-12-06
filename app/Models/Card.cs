3e788e7efd574b56e8a7ceb8da6d7f1c820dfb94
sergioLahuerta <64411350+sergioLahuerta@users.noreply.github.com>
2025-12-06 12:45:38 +0100

    Rename Card model property 'Name' to 'CardName' for clarity

--- app/Models/Card.cs
+++ app/Models/Card.cs
@@ -2,7 +2,7 @@ namespace swuApi.Models {
     public class Card
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string CardName { get; set; }
         public string Subtitle { get; set; }
         public string Model { get; set; }
         public string Aspect { get; set; }
