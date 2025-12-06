f7852e3e2c39582e005d16dfbc4051eabf969f15
sergioLahuerta <64411350+sergioLahuerta@users.noreply.github.com>
2025-12-06 16:22:28 +0100

    Edited card model

--- swuApi/Models/Card.cs
+++ swuApi/Models/Card.cs
@@ -1,16 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace swuApi.Models
 {
     public class Card
     {
-        public int Id { get; set; }
-        public string CardName { get; set; }
+        public int Id { get; set; } 
+        public string CardName { get; set; } = string.Empty;
         public string Subtitle { get; set; }
-        public string Model { get; set; }
-        public string Aspect { get; set; }
-        public int CardNumber { get; set; }
+        public string Model { get; set; } = string.Empty;
+        public string Aspect { get; set; } 
+        public int CardNumber { get; set; } 
         public int Copies { get; set; }
-        public int ColectionId { get; set; }
-        // public Colection? Colection { get; set; }
-        public Card() { }
+        public decimal Price { get; set; } 
+        public DateTime DateAcquired { get; set; } 
+        public bool IsPromo { get; set; } 
+        public int CollectionId { get; set; }
+        public Colection? Colection { get; set; }
+
+        public Card() 
+        {
+            DateAcquired = DateTime.Now;
+        }
     }
 }
