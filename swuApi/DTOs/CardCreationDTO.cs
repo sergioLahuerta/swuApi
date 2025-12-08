a8ffe42518c55d01bb750bd1a8c7fefa4808df33
sergioLahuerta <64411350+sergioLahuerta@users.noreply.github.com>
2025-12-08 15:36:39 +0100

    dtos added

--- swuApi/DTOs/CardCreationDTO.cs
+++ swuApi/DTOs/CardCreationDTO.cs
@@ -1,22 +1,7 @@
-using System;
-using System.ComponentModel;
-using swuApi.Enums;
-
 namespace swuApi.DTOs
 {
-    public class CardCreationDTO
+    public class CardCreationDTO : ICardDTO
     {
-        public string CardName { get; set; } = string.Empty;
-        public string? Subtitle { get; set; }
-        public CardModelType Model { get; set; }
-        public CardAspectType Aspect { get; set; }
-        public CardRarityType Rarity { get; set; } = CardRarityType.Common;
-        public int CardNumber { get; set; }
-        public decimal Price { get; set; }
-        public DateTime DateAcquired { get; set; } = default;
         
-        [DefaultValue(false)]
-        public bool IsPromo { get; set; }
-        public int CollectionId { get; set; }
     }
 }
