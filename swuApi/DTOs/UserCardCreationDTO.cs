e47febcf689ade45a9f5dc812c1cc1c74a046363
sergioLahuerta <64411350+sergioLahuerta@users.noreply.github.com>
2025-12-08 17:49:00 +0100

    deleted duplicated dto

--- swuApi/DTOs/UserCardCreationDTO.cs
+++
@@ -1,7 +0,0 @@
-public class UserCardCreationDTO
-{
-    public int UserId { get; set; }
-    public int CardId { get; set; }
-    public int Copies { get; set; } = 1;
-    public bool IsFavorite { get; set; } = false;
-}
